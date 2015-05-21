38129f11c044e0b3415602c3602e15d4f0e192c3
j129008 <j129008@gmail.com>
2015-05-21 17:47:42 +0800

    結束index 初步版本 並且把檔案移動到外面(css, js) TODO 改良難看的UserData輸入 解決頁面縮小的問題(RWD)

---
+++ js/index.js
@@ -0,0 +1,88 @@
+(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+ga('create', 'UA-61390412-1', 'auto');
+ga('send', 'pageview');
+window.fbAsyncInit = function() {
+   FB.init({
+appId      : '439749989534358',
+xfbml      : true,
+version    : 'v2.3'
+});
+};
+
+(function(d, s, id){
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) {return;}
+    js = d.createElement(s); js.id = id;
+    js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=439749989534358&version=v2.3";
+    fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+function myScroll(id){
+    $('html,body').animate(
+            {scrollTop: $("#"+id).offset().top-50},
+            'slow'
+            )
+}
+
+var ansList = [];
+function saveAnsAndFadeOut( Qx, Ans, Show ){
+     $('#'+Qx).fadeOut();
+     $('#'+Show).fadeIn();
+     ansList.push(Ans);
+ }
+
+ $(document).ready(
+     function(){
+         $('#Q1_2,#Q1_1,#Q2,#eye,#cold,#skin,#pee,#bone,#belly,#ear,#userData').hide();
+     }
+ );
+ $(window).load(
+     function(){
+         $('#Q1_2,#Q1_1,#Q2,#eye,#cold,#skin,#pee,#bone,#belly,#ear,#userData').hide();
+     }
+ );
+
+function post(params) {
+
+    for(var key in params) {
+        if(params.hasOwnProperty(key)) {
+            var hiddenField = document.createElement("input");
+            hiddenField.setAttribute("type", "hidden");
+            hiddenField.setAttribute("name", key);
+            hiddenField.setAttribute("value", params[key]);
+
+            form.appendChild(hiddenField);
+        }
+    }
+
+    form.submit();
+}
+
+var form = document.createElement("form");
+form.setAttribute("method", "POST");
+form.setAttribute("action", "http://pmck.comlu.com/pmck.php");
+form.setAttribute("class", "form-inline");
+form.setAttribute("role", "form");
+
+function addFormInput(formLabel, value){
+    var formGroupDiv = document.createElement("div");
+    formGroupDiv.setAttribute("class","form-group");
+
+    var inputTitle = document.createElement("label");
+    var text = document.createTextNode(formLabel);
+    inputTitle.appendChild(text);
+
+    var visibilityField = document.createElement("input");
+    visibilityField.setAttribute("name", value);
+    visibilityField.setAttribute("type", "text");
+    visibilityField.setAttribute("class", "form-control");
+
+    form.appendChild(formGroupDiv);
+    formGroupDiv.appendChild(inputTitle);
+    formGroupDiv.appendChild(visibilityField);
+    document.getElementById("ans").appendChild(form);
+}
