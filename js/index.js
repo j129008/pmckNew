b71bcfc5136acc97edb21020cbbab352638288f3
j129008 <j129008@gmail.com>
2015-05-29 21:14:15 +0800

    add fb like button

--- js/index.js
+++ js/index.js
@@ -13,14 +13,6 @@ version    : 'v2.3'
 });
 };
 
-//(function(d, s, id){
-    //var js, fjs = d.getElementsByTagName(s)[0];
-    //if (d.getElementById(id)) {return;}
-    //js = d.createElement(s); js.id = id;
-    //js.src = "//connect.facebook.net/zh_TW/sdk.js#xfbml=1&appId=439749989534358&version=v2.3";
-    //fjs.parentNode.insertBefore(js, fjs);
-//}(document, 'script', 'facebook-jssdk'));
-
 function myScroll(id){
     $('html,body').animate(
             {scrollTop: $("#"+id).offset().top-50},
