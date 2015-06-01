acae49976c64e40dc68f620bb3ce609b252e277a
j129008 <j129008@gmail.com>
2015-06-01 19:37:09 +0800

    讓panel縮的回去

--- js/panel.js
+++ js/panel.js
@@ -6,6 +6,12 @@ $(window).ready(
 
 $(".panel").click(
         function(){
-            $(".panel-body",this).slideDown();
-            $(".panel-footer",this).slideDown();
+            var $t = $('.panel-body',this);
+            if($t.is(':visible') == false){
+                $(".panel-body",this).slideDown();
+                $(".panel-footer",this).slideDown();
+            }else{
+                $(".panel-body",this).slideUp();
+                $(".panel-footer",this).slideUp();
+            }
         });
