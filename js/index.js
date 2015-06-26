acba80fb5f411f7fe33afeda990a4f5c1d63bfd3
j129008 <j129008@gmail.com>
2015-06-26 22:07:02 +0800

    加入手機檢查, 以及長度設定

--- js/index.js
+++ js/index.js
@@ -93,7 +93,7 @@ form.setAttribute("class", "form");
 form.setAttribute("role", "form");
 form.setAttribute("data-toggle", "validator");
 
-function addFormInput(formLabel, value, regex){
+function addFormInput(formLabel, value, regex, len){
     var formGroupDiv = document.createElement("div");
     formGroupDiv.setAttribute("class","form-group");
     formGroupDiv.setAttribute("style","height: 50px;");
@@ -105,6 +105,7 @@ function addFormInput(formLabel, value, regex){
     visibilityField.setAttribute("placeholder", value);
     visibilityField.setAttribute("pattern", regex);
     visibilityField.setAttribute("required", "required");
+    visibilityField.setAttribute("maxlength", len);
 
     form.appendChild(formGroupDiv);
     formGroupDiv.appendChild(visibilityField);
@@ -129,10 +130,10 @@ function addCheckBox(label){
 
 
 $(function(){
-    addFormInput('姓名：', '姓名','[^A-Za-z0-9!@#$%^&\\*\\(\\)_+\\[\\]\\s\\.\\\\<>{},/?\\";\\\':-|~]+');
-    addFormInput('手機：', '手機','(^09[0-9]{8}|^09[0-9]{2}-[0-9]{6})');
-    addFormInput('信箱：', '信箱','.+@.+');
-    addFormInput('留言：', '留言','.+');
+    addFormInput('姓名：', '姓名','[^A-Za-z0-9!@#$%^&\\*\\(\\)_+\\[\\]\\s\\.\\\\<>{},/?\\";\\\':-|~]+',4);
+    addFormInput('手機：', '手機','^09(11|12|13|14|15|16|17|18|19|20|21|22|23|24|25|26|27|28|29|30|31|32|33|34|35|36|37|38|39|52|53|54|55|56|58|60|61|63|66|68|70|71|72|73|75|76|77|78|79|80|81|82|83|84|85|86|87|88|89|03|05|09)[0-9]{6}',10);
+    addFormInput('信箱：', '信箱','.+@.+',50);
+    addFormInput('留言：', '留言','.+',1000);
     addCheckBox("是住在基隆地區的飼主");
     addCheckBox("接受過治療但效果不彰");
     var btn = document.createElement("button");
